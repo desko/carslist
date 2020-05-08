73808e0a5e4e5060eefb1df86a4695c0bc638844
desko <Prox@DESKTOP-KKLLFAT>
2020-05-08 19:54:19 +0300

    Working modals for the front page

--- kursova/src/main/webapp/index.php
+++ kursova/src/main/webapp/index.php
@@ -25,54 +25,7 @@
 		</header>
 		<div class="main">
 			<div class="newPostContainer">
-        		<?php
-                    $db = mysqli_connect('localhost', 'root', 'root', 'carsdb');
-        			$query="SELECT * FROM listings ORDER BY idlisting DESC LIMIT 10";
-        			$result = mysqli_query($db, $query);
-        			$row = array();
-        			$dir="dirpicture1";
-        			$imgdirstart="<img class=\"searchImage\" src=\"/uploads/";
-        			if (mysqli_num_rows($result) > 0)
-        			{
-        			    while($row = mysqli_fetch_assoc($result)) {
-        			        $item = array();
-        			        $item['idlisting']           = $row['idlisting'];
-        			        $item['listingowner']        = $row['listingowner'];
-        			        $item['description']         = $row['description'];
-        			        $item['dirpicture1']         = $row['dirpicture1'];
-        			        $item['manufacturer']        = $row['manufacturer'];
-        			        $item['model']               = $row['model'];
-        			        $item['price']               = $row['price'];
-        			        $returnData[] = $item;
-        			        echo "<div id=\"eachResultContainerID\" class=\"eachResultContainer\" onclick=\"displayModal(".$item['idlisting'].")\">";
-        			        echo $item["manufacturer"]." ".$item["model"]."<br>".$item["price"]."<br>";
-        			        echo $imgdirstart.$item[$dir]."\">";
-        			        echo "</div>";
-        			        echo "<div id=".$item['idlisting']." class=\"modal\">
-                                    <div class=\"modal-content\">
-                                        <div class=\"modal-header\">
-                                            <span class=\"close\" onclick=\"closeModal(".$item['idlisting'].")\">&times;</span>
-                                            <h2>";
-        			        echo $item["manufacturer"]." ".$item["model"]." ".$item["price"]." lv";
-                            echo
-                                            '</h2>
-                                        </div>
-                                        <div class="modal-body">
-                                            <p>Some text in the Modal Body</p>
-                                            <p>Some other text...</p>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <h3>Modal Footer</h3>
-                                        </div>
-                                    </div>
-                                </div>';
-        			    }
-        			}
-        			else
-        			{
-        			    echo "0 results";
-        			}
-        		?>
+        		<?php include('indexresmodal.php')?>
         	</div>
 		</div>
 	</div>
